90eda455c130044f2bafe349a039cd8b6d2996fc
Agustin Karkocki <agustin.karkocki@gmail.com>
2022-07-14 17:43:19 -0300

    added projects section

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -84,7 +84,7 @@ export const SectionDivider = styled.div`
   background-color: #fff;
   background: ${(props) => props.colorAlt ?
     'linear-gradient(270deg, #F46737 0%, #945DD6 100%)' :
-    'linear-gradient(270deg, #13ADC7 0%, #945DD6 100%)'};
+    'linear-gradient(270deg, #61045F 0%, #AA076B 100%)'};
 
     margin: ${(props) => props.divider ? "4rem 0" : ""};
 
